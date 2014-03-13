dd100b24dc95ae9d918c58641d0de4bc80ea2105
stefanand <stefan.andersson@live.com>
2014-03-13 22:56:11 +0100

    Changed qxe StatusBar Demo.

---
+++ trunk/demo/StatusBar/Gruntfile.js
@@ -0,0 +1,38 @@
+// requires
+var util = require('util');
+var qx = require("../../../qooxdoo/tool/grunt");
+
+// grunt
+module.exports = function(grunt) {
+  var config = {
+
+    generator_config: {
+      let: {
+      }
+    },
+
+    common: {
+      "APPLICATION" : "qxe",
+      "QOOXDOO_PATH" : "../../../qooxdoo",
+      "LOCALES": ["en"],
+      "QXTHEME": "qxe.theme.Theme"
+    }
+
+    /*
+    myTask: {
+      options: {},
+      myTarget: {
+        options: {}
+      }
+    }
+    */
+  };
+
+  var mergedConf = qx.config.mergeConfig(config);
+  // console.log(util.inspect(mergedConf, false, null));
+  grunt.initConfig(mergedConf);
+
+  qx.tasks.registerTasks(grunt);
+
+  // grunt.loadNpmTasks('grunt-my-plugin');
+};
